ee88b2ee78267ff3b97ca6a2a2ce706c0b629c16
Shivam Mourya <mouryas318@gmail.com>
2024-10-12 00:57:43 +0530

    bkr

--- backend/src/routes/CommunityFourm.js
+++ backend/src/routes/CommunityFourm.js
@@ -1,11 +1,13 @@
 import express from "express";
 import CategoryController from "../controller/communityFourm/CategoryController.js";
 import TopicController from "../controller/communityFourm/TopicsController.js";
+import replyController from "../controller/communityFourm/ReplyController.js";
 import {upload} from "../middlewares/multer.middleware.js"
 
 const router = express.Router();
 
 router.post("/:userId/createCategory", CategoryController);
 router.post("/:categoryId/topics",upload.single('file'),TopicController);
+router.post("/:userId/:topicId/reply", replyController )
 
 export default router;
