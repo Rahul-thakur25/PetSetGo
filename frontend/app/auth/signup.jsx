74ca0707c4c0fc140d2c0ed0d6d304551db10f21
Sktiwary9122 <sktiwary9122@gmail.com>
2024-10-14 19:59:17 +0530

    craeted 1st page

--- frontend/app/auth/signup.jsx
+++ frontend/app/auth/signup.jsx
@@ -1,12 +1,92 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ScrollView,Image, Alert } from 'react-native'
+import React,{useState} from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+// import { images } from '../../constants'
+import { Link, router } from 'expo-router'
+import FormField from '../../components/FormField'
+import CustomButton from '../../components/CustomButton'
 
-const signup = () => {
+const Signup = () => {
+    const [form, setForm] = useState({
+        username:'',
+        email:'',
+        password:'',
+    })
+    const [isSubmitting, setisSubmitting] = useState(false)
+    const submit=async() => {
+      if(!form.username || !form.email || !form.password){
+        Alert.alert('Error','Please fill in all the fields');
+      }
+      setisSubmitting(true)
+      try {
+        const result = await createUser(form.email,form.password,form.username)
+        router.replace('/Home')
+      } catch (error) {
+        Alert.alert('Error',error.message)
+      }finally{
+        setisSubmitting(false)
+      }
+     
+    }
+
   return (
-    <View>
-      <Text>signup</Text>
-    </View>
-  );
-};
+    <SafeAreaView className='bg-[#161622] h-full '>
+      <ScrollView>
+        <View className="w-full min-h-[85vh] justify-center px-4 my-6">
+            {/* <Image 
+            source={images.logo}
+            resizeMode='contain'
+            className="w-[115px] h-[35px]"
+            ></Image> */}
+            <Text className="text-2xl text-white mt-10 font-psemibold text-semibold">Sign up to Pet-Set-Go</Text>
+            <FormField
+                title='User Name'
+                value={form.username}
+                handleChangeText={(e)=>{
+                    setForm({...form,
+                    username:e})
+                }}
+                placeholder='Enter Username'
+                otherStyles="mt-10"
+            />
+            <FormField
+                title='Email'
+                value={form.email}
+                handleChangeText={(e)=>{
+                    setForm({...form,
+                    email:e})
+                }}
+                placeholder='Enter Email'
+                otherStyles="mt-7"
+                keyboardType="email-address"
+            />
+            <FormField
+                title='Password'
+                value={form.password}
+                handleChangeText={(e)=>{
+                    setForm({...form,
+                    password:e})
+                }}
+                placeholder='Enter Password'
+                otherStyles="mt-7"
+            />
 
-export default signup;
+            <CustomButton
+                title="Sign Up"
+                handlePress={submit}
+                containerStyles="mt-7"
+                isLoading={isSubmitting}
+            />
+            <View className="justify-center pt-5 flex-row gap-2">
+                <Text className="text-lg text-gray-100 font-pregular">
+                    Already have an account?
+                </Text>
+                <Link href="Sign-in" className="text-lg font-semibold text-[#4DB6AC]">Sign In</Link>
+            </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default Signup
---
+++ frontend/components/FormField.jsx
@@ -0,0 +1,36 @@
+import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native'
+import React,{useState} from 'react'
+// import { icons } from '../constants'
+
+const FormField = ({title,value,placeholder,handleChangeText,otherStyles,...props}) => {
+
+    const [showPassword, setshowPassword] = useState(false)
+  return (
+    <View className={`space-y-2 ${otherStyles}`}>
+      <Text className="text-base text-gray-100 font-pmedium" >{title}</Text>
+
+      <View className="w-full h-16 px-4 bg-black-100 border border-gray-300 flex-row  rounded-2xl focus:border-[#FF8A65] items-center">
+        <TextInput 
+            className="flex-1 text-white font-psemibold text-base w-full"
+            value={value}
+            placeholder={placeholder}
+            placeholderTextColor="#7b7b8b"
+            onChangeText = {handleChangeText}
+            secureTextEntry = {title === 'Password' && !showPassword }
+        />
+
+        {title === 'Password' && (
+            <TouchableOpacity onPress={() => setshowPassword(!showPassword)}>
+              {/* <Image source={!showPassword? icons.eye : icons.eyeHide}
+                className = "w-6 h-6 "
+                resizeMode='contain'
+              /> */}
+              
+            </TouchableOpacity>
+        )}
+      </View>
+    </View>
+  )
+}
+
+export default FormField
